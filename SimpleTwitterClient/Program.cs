d5524932cfc852a0f708594c042d9a39d734fa1b
Matt Carroll <tayopi8@gmail.com>
2017-10-05 17:22:17 -0600

    Add project files.

---
+++ SimpleTwitterClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTwitterClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What are you looking for? ");
+            string searchValue = Console.ReadLine();
+
+
+        }
+    }
+}
